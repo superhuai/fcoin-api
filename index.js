a1a89036f9c928ac69e73eb21a82c2f3d68ecd2a
imconfig <imconfig@aliyun.com>
2018-07-10 17:19:50 +0800

    init

--- index.js
+++ index.js
@@ -1,176 +1,211 @@
-const fetch = require('node-fetch');
-const crypto = require('crypto');
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const node_fetch_1 = tslib_1.__importDefault(require("node-fetch"));
+const crypto_1 = tslib_1.__importDefault(require("crypto"));
 const API = {
-    order: "https://api.fcoin.com/v2/orders",
-    market: "wss://api.fcoin.com/v2/ws",
-    market_http: "https://api.fcoin.com/v2/market/",
-    balance: "https://api.fcoin.com/v2/accounts/balance"
-}
-// Util Func
-
-class Fcoin {
-    constructor(obj) {
-        this.config = obj;
-    }
-    getconfig() {
-        console.log(this.config);
-    }
-    // 工具类
-    tob64(str) {
-        return new Buffer(str).toString('base64')
-    }
-    secret(str) {
-        str = this.tob64(str);
-        str = crypto.createHmac('sha1', this.config.secret).update(str).digest().toString('base64');
-        return str;
+    order: 'https://api.fcoin.com/v2/orders',
+    market: 'wss://api.fcoin.com/v2/ws',
+    market_http: 'https://api.fcoin.com/v2/market/',
+    balance: 'https://api.fcoin.com/v2/accounts/balance',
+};
+class FcoinApi {
+    constructor(key, secret) {
+        this.Config = {
+            Key: '',
+            Secret: '',
+        };
+        this.Config.Key = key;
+        this.Config.Secret = secret;
     }
     /**
      * 创建订单（买卖）
-     * @param {交易对} symbol 
-     * @param {买卖方向} side 
-     * @param {现价还是市价} type 
-     * @param {价格, string} price 
-     * @param {数量, string} amount 
      */
-    createOrder(symbol, side, type = 'limit', price, amount = 0.01) {
-        let time = Date.now();
-        let signtmp = this.secret(`POST${API.order}${time}amount=${amount}&price=${price}&side=${side}&symbol=${symbol}&type=${type}`);
-    
-        // 发送新建订单的请求
-        return fetch(API.order, {
-        method: 'post',
-        headers: {
-            'FC-ACCESS-KEY': this.config.key,
-            'FC-ACCESS-SIGNATURE': signtmp,
-            'FC-ACCESS-TIMESTAMP': time,
-            'Content-Type': 'application/json;charset=UTF-8'
-        },
-        body: JSON.stringify({
-            "type": type,
-            "side": side,
-            "amount": amount,
-            "price": price,
-            "symbol": symbol
-        })
-    
-        }).then(res => res.json())
-        
+    OrderCreate(symbol, side, type, price, amount) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            const time = Date.now().toString();
+            const signtmp = this.secret(`POST${API.order}${time}amount=${amount}&price=${price}&side=${side}&symbol=${symbol}&type=${type}`);
+            return node_fetch_1.default(API.order, {
+                method: 'post',
+                headers: {
+                    'FC-ACCESS-KEY': this.Config.Key,
+                    'FC-ACCESS-SIGNATURE': signtmp,
+                    'FC-ACCESS-TIMESTAMP': time,
+                    'Content-Type': 'application/json;charset=UTF-8',
+                },
+                body: JSON.stringify({ type, side, amount, price, symbol }),
+            }).then(res => res.json());
+        });
     }
-    // 撤销订单（买卖）
     /**
-     * 
-     * @param {订单id} id 
+     * 撤销订单（买卖）
      */
-    cancelOrder(id) {
-        let time = Date.now();
-        let url = `${API.order}/${id}/submit-cancel`;
-        let signtmp = this.secret(`POST${url}${time}`);
-    
+    OrderCancel(id) {
+        const time = Date.now().toString();
+        const url = `${API.order}/${id}/submit-cancel`;
+        const signtmp = this.secret(`POST${url}${time}`);
         // 发送新建订单的请求
-        return fetch(url, {
-        method: 'post',
-        headers: {
-            'FC-ACCESS-KEY': this.config.key,
-            'FC-ACCESS-SIGNATURE': signtmp,
-            'FC-ACCESS-TIMESTAMP': time,
-            'Content-Type': 'application/json;charset=UTF-8'
-        }
-    
-        }).then(res => res.json())
+        return node_fetch_1.default(url, {
+            method: 'post',
+            headers: {
+                'FC-ACCESS-KEY': this.Config.Key,
+                'FC-ACCESS-SIGNATURE': signtmp,
+                'FC-ACCESS-TIMESTAMP': time,
+                'Content-Type': 'application/json;charset=UTF-8',
+            },
+        }).then(res => res.json());
     }
     // 查询账户资产
-    getBalance() {
-        let time = Date.now();
-        let signtmp = this.secret(`GET${API.balance}${time}`)
-        console.log(signtmp,API.balance)
-        return fetch(API.balance, {
+    FetchBalance() {
+        const time = Date.now().toString();
+        const signtmp = this.secret(`GET${API.balance}${time}`);
+        return node_fetch_1.default(API.balance, {
             method: 'GET',
             headers: {
-                'FC-ACCESS-KEY': this.config.key,
+                'FC-ACCESS-KEY': this.Config.Key,
                 'FC-ACCESS-SIGNATURE': signtmp,
-                'FC-ACCESS-TIMESTAMP': time
-            }
-        }).then(res => res.json())
-        
+                'FC-ACCESS-TIMESTAMP': time,
+            },
+        }).then(res => res.json());
     }
     // 查询所有订单
-    /**
-     * 
-     * @param {交易对} symbol 
-     * @param {订单状态} states 
-     * @param {每页限制数量, string} limit 
-     * @param {在某个时间戳之后, string} after 
-     * @param {在某个时间戳之前, string} before 
-     */
-    getOrders(symbol, states = 'submitted,filled', limit = '100', after="", before = "") {
-        let time = Date.now();
+    FetchOrders(symbol, states = 'submitted,filled', limit = '100', after = '', before = '') {
+        const time = Date.now().toString();
         let url;
-        if(before){
-          url = `${API.order}?before=${before}&limit=${limit}&states=${states}&symbol=${symbol}`;
-        } else if(after){
-          url = `${API.order}?after=${after}&limit=${limit}&states=${states}&symbol=${symbol}`;
-        } else {
-          url = `${API.order}?limit=${limit}&states=${states}&symbol=${symbol}`;
+        if (before) {
+            url = `${API.order}?before=${before}&limit=${limit}&states=${states}&symbol=${symbol}`;
         }
-        
-        let signtmp = this.secret(`GET${url}${time}`)
-        return fetch(url, {
-        method: 'GET',
-        headers: {
-            'FC-ACCESS-KEY': this.config.key,
-            'FC-ACCESS-SIGNATURE': signtmp,
-            'FC-ACCESS-TIMESTAMP': time
+        else if (after) {
+            url = `${API.order}?after=${after}&limit=${limit}&states=${states}&symbol=${symbol}`;
         }
-        }).then(res => res.json())
-        
+        else {
+            url = `${API.order}?limit=${limit}&states=${states}&symbol=${symbol}`;
+        }
+        const signtmp = this.secret(`GET${url}${time}`);
+        return node_fetch_1.default(url, {
+            method: 'GET',
+            headers: {
+                'FC-ACCESS-KEY': this.Config.Key,
+                'FC-ACCESS-SIGNATURE': signtmp,
+                'FC-ACCESS-TIMESTAMP': time,
+            },
+        }).then(res => res.json());
     }
-    // 获取指定 id 的订单 
-    /**
-     * 
-     * @param {订单id} id 
-     */
-    getOrderByid(id) {
-        let time = Date.now();
-        let url = `${API.order}/${id}`;
-        let signtmp = this.secret(`GET${url}${time}`)
-        return fetch(url, {
-        method: 'GET',
-        headers: {
-            'FC-ACCESS-KEY': this.config.key,
-            'FC-ACCESS-SIGNATURE': signtmp,
-            'FC-ACCESS-TIMESTAMP': time
-        }
-        }).then(res => res.json())
-        
+    // 获取指定 id 的订单
+    FetchOrderById(id) {
+        const time = Date.now().toString();
+        const url = `${API.order}/${id}`;
+        const signtmp = this.secret(`GET${url}${time}`);
+        return node_fetch_1.default(url, {
+            method: 'GET',
+            headers: {
+                'FC-ACCESS-KEY': this.Config.Key,
+                'FC-ACCESS-SIGNATURE': signtmp,
+                'FC-ACCESS-TIMESTAMP': time,
+            },
+        }).then(res => res.json());
     }
     /**
      * 行情接口(ticker)
-     * @param {交易对} symbol 
      */
-    getTicker(symbol) {
-    
+    Ticker(symbol) {
         let url = `${API.market_http}/ticker/${symbol}`;
-    
-        return fetch(url, {
-        method: 'GET'
-        }).then(res => res.json())
-        
+        return node_fetch_1.default(url, {
+            method: 'GET',
+        }).then(res => res.json()).then((res) => {
+            if (res.status)
+                return res;
+            const ticker = res.data.ticker;
+            return new FcoinApiRes({
+                seq: res.data.seq,
+                type: res.data.type,
+                LastPrice: ticker[0],
+                LastVolume: ticker[1],
+                MaxBuyPrice: ticker[2],
+                MaxBuyVolume: ticker[3],
+                MinSalePrice: ticker[4],
+                MinSaleVolume: ticker[5],
+                BeforeH24Price: ticker[6],
+                HighestH24Price: ticker[7],
+                LowestH24Price: ticker[8],
+                OneDayVolume1: ticker[9],
+                OneDayVolume2: ticker[10],
+            });
+        });
     }
     /**
      * 深度查询
-     * @param {L20 default} deep 
-     * @param {交易对} symbol 
      */
-    getDepth(deep, symbol) {
-    
-        let url = `${API.market_http}/depth/${deep}/${symbol}`;
-    
-        return fetch(url, {
-        method: 'GET'
-        }).then(res => res.json())
-        
+    Depth(depth, symbol) {
+        const url = `${API.market_http}/depth/${depth}/${symbol}`;
+        return node_fetch_1.default(url, {
+            method: 'GET',
+        }).then(res => res.json()).then((res) => {
+            if (res.status)
+                return res;
+            const bids = [];
+            const asks = [];
+            res.data.bids.forEach((num, index) => {
+                const isVol = Boolean(index % 2);
+                const realIndex = Math.floor(index / 2);
+                if (!isVol) {
+                    bids.push({ Price: num, Volume: 0 });
+                }
+                else {
+                    bids[realIndex].Volume = num;
+                }
+            });
+            res.data.asks.forEach((num, index) => {
+                const isVol = Boolean(index % 2);
+                const realIndex = Math.floor(index / 2);
+                if (!isVol) {
+                    asks.push({ Price: num, Volume: 0 });
+                }
+                else {
+                    asks[realIndex].Volume = num;
+                }
+            });
+            return new FcoinApiRes({
+                bids, asks,
+                seq: res.data.seq,
+                ts: res.data.ts,
+                type: res.data.type,
+            });
+        });
+    }
+    // 工具类
+    tob64(str) {
+        return new Buffer(str).toString('base64');
+    }
+    secret(str) {
+        str = this.tob64(str);
+        str = crypto_1.default.createHmac('sha1', this.Config.Secret).update(str).digest().toString('base64');
+        return str;
     }
 }
-
-module.exports = Fcoin;
+exports.FcoinApi = FcoinApi;
+class FcoinApiRes {
+    constructor(data) {
+        this.status = 0;
+        this.data = data;
+    }
+}
+exports.FcoinApiRes = FcoinApiRes;
+/**
+ * @param {已提交} submitted
+ * @param {部分成交} partial_filled
+ * @param {部分成交已撤销} partial_canceled
+ * @param {完全成交} filled
+ * @param {已撤销} canceled
+ * @param {撤销已提交} pending_cancel
+ */
+var OrderState;
+(function (OrderState) {
+    OrderState["submitted"] = "submitted";
+    OrderState["partial_filled"] = "partial_filled";
+    OrderState["partial_canceled"] = "partial_canceled";
+    OrderState["filled"] = "filled";
+    OrderState["canceled"] = "canceled";
+    OrderState["pending_cancel"] = "pending_cancel";
+})(OrderState = exports.OrderState || (exports.OrderState = {}));
+//# sourceMappingURL=index.js.map
